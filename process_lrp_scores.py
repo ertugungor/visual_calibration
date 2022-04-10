af8eb3d6b122556ae04403800f18c03d3d13fb74
Ertugrul Gungor <ertugungor@gmail.com>
2022-04-11 02:03:12 +0300

    Update analyzer scripts to work with new data format including experiment type
    Data files are fetched from remote server and not stored in the git for now.
    In the future, they might be stored as LFS files.

--- process_lrp_scores.py
+++ process_lrp_scores.py
@@ -6,10 +6,12 @@ from lvis_cls_mapping import index_to_freq_group
 from lvis_cls_mapping import CLASSES
 from os import path
 from os import mkdir
+from os import mkdir, path, remove
 
-RESULTS_PATH_PREFIX = "/workspace/lrp_score_plots/data/calibration/iterations"
-SLRP_STATS_PATH_PREFIX = "/workspace/lrp_score_plots/slrp/stats"
-SLRP_PLOTS_PATH_PREFIX = "/workspace/lrp_score_plots/slrp/plots/"
+# These paths are based on the container mounting configuration in devcontainer.json file
+INPUT_DATA_PATH_PREFIX = "/workspace/visual_calibration/data/"
+RESULTS_STATS_PATH_PREFIX = "/workspace/visual_calibration/data/analysis/stats"
+RESULTS_PLOTS_PATH_PREFIX = "/workspace/visual_calibration/data/analysis/plots"
 
 def transform(data, type):
   if type == "lrp":
@@ -20,12 +22,13 @@ def transform(data, type):
     data = np.insert(data, len(data), 0)
   return data
 
-def read_data(iterations, data_keys, dataset_types):
+def read_data(iterations, data_keys, dataset_types, model_dataset_type):
   iter_values_map = defaultdict(defaultdict)
   for iter in iterations:
     for dataset_type in dataset_types:
       for key in data_keys:
-        file_name = path.join(RESULTS_PATH_PREFIX, str(iter), f"{dataset_type}", f"{dataset_type}_{key}")
+        file_name = path.join(INPUT_DATA_PATH_PREFIX, model_dataset_type, f"{dataset_type}", str(iter), "eval", f"{dataset_type}_{key}")
+        print(file_name)
         with open(file_name, 'rb') as in_file:
           iter_values_map[iter][f"{dataset_type}_{key}"] = pickle.load(in_file)
   return iter_values_map
@@ -49,16 +52,25 @@ def draw_slrp_plot(x_points, y_points, colors, curve_labels, axis_labels, title,
   plt.savefig(fig_name)
   plt.cla()
 
-def create_plots(iterations):
-  data_keys = ["lrp_values", "dt_scores", "tps", "fps"]
+"""
+Creates sLRP plots for given results for given experiment
+
+Args:
+
+iterations: For which iterations, the plots will be generated. Array of integers denoting iterations.
+model_dataset_type: Keywords defining the experiment, e.g. mask_rcnn_lvis
+"""
+def create_plots(iterations, model_dataset_type):
+  data_keys = ["lrp_values", "dt_scores"]
   dataset_types = ["val", "train"]
-  results_data = read_data(iterations, data_keys, dataset_types)
+  results_data = read_data(iterations, data_keys, dataset_types, model_dataset_type)
+  print("data read")
   for iter in iterations:
     values = results_data[iter]
-    val_lrp_values, val_dt_scores, val_tps, val_fps = values[0],values[1],values[2],values[3]
-    train_lrp_values, train_dt_scores, train_tps, train_fps = values[4],values[5],values[6],values[7]
+    val_lrp_values, val_dt_scores = values["val_lrp_values"], values["val_dt_scores"]
+    train_lrp_values, train_dt_scores = values["train_lrp_values"], values["train_dt_scores"]
 
-    dir_path = path.join(SLRP_PLOTS_PATH_PREFIX, str(iter))
+    dir_path = path.join(RESULTS_PLOTS_PATH_PREFIX, str(iter))
     if not path.isdir(dir_path):
       mkdir(dir_path)
 
@@ -120,15 +132,23 @@ def create_plots(iterations):
         curve_labels.append("val")
       draw_slrp_plot(x_points, y_points, colors, curve_labels, axis_labels, title, fig_name)
 
-def obtain_slrp_stats(iterations, count_settings):
+def obtain_slrp_stats(iterations, model_dataset_type, count_settings):
   for iter in iterations:
     for dataset_type in ["train", "val"]:
-      slrp_result_path = file_name = path.join(RESULTS_PATH_PREFIX, str(iter), f"{dataset_type}", f"{dataset_type}_lrp_opt_thr")
+      slrp_result_path = path.join(INPUT_DATA_PATH_PREFIX, model_dataset_type, f"{dataset_type}", str(iter), "eval", f"{dataset_type}_lrp_opt_thr")
+      print(slrp_result_path)
       with open(slrp_result_path, 'rb') as in_file:
         lrp_opt_thrs = pickle.load(in_file)
 
       num_nans = { "rare":0, "common":0, "frequent":0}
       num_negs = { "rare":0, "common":0, "frequent":0}
+
+      dir_path = path.join(RESULTS_STATS_PATH_PREFIX, str(iter))
+      stats_out_path = path.join(dir_path, f"{dataset_type}_opt_thr_stats.txt")
+      if not path.isdir(dir_path):
+        mkdir(dir_path)
+      if path.isfile(stats_out_path):
+        remove(stats_out_path)
 
       print_count_per_cat_info = True
       for (count_nans,count_negs) in count_settings:
@@ -162,10 +182,6 @@ def obtain_slrp_stats(iterations, count_settings):
         else:
           info = "Ignoring both"
 
-        dir_path = path.join(SLRP_STATS_PATH_PREFIX, str(iter))
-        stats_out_path = path.join(dir_path, f"{dataset_type}_opt_thr_stats.txt")
-        if not path.isdir(dir_path):
-          mkdir(dir_path)
         with open(stats_out_path, 'a') as out_file:
           if print_count_per_cat_info:
             print_count_per_cat_info = False
@@ -195,12 +211,12 @@ def draw_slrp_opt_plot(x_points, y_points, axis_labels, title, fig_name):
   plt.savefig(fig_name)
   plt.cla()
 
-def obtain_slrp_scatter_plots(iterations):
+def obtain_slrp_scatter_plots(iterations, model_dataset_type):
   for iter in iterations:
-    train_lrp_opt_thr_path = path.join(RESULTS_PATH_PREFIX, str(iter), "train", "train_lrp_opt_thr")
-    val_lrp_opt_thr_path = path.join(RESULTS_PATH_PREFIX, str(iter), "val", "val_lrp_opt_thr")
+    train_lrp_opt_thr_path = path.join(INPUT_DATA_PATH_PREFIX, model_dataset_type, "train", str(iter), "eval", "train_lrp_opt_thr")
+    val_lrp_opt_thr_path = path.join(INPUT_DATA_PATH_PREFIX, model_dataset_type, "val", str(iter), "eval", "val_lrp_opt_thr")
 
-    dir_path = path.join(SLRP_STATS_PATH_PREFIX, str(iter))
+    dir_path = path.join(RESULTS_STATS_PATH_PREFIX, str(iter))
     if not path.isdir(dir_path):
       mkdir(dir_path)
 
@@ -283,17 +299,17 @@ def obtain_score_distribution(iterations, data_keys, dataset_types):
             ["cat_id", "avg_score"], "Val set score distribution wrt iterations",
             "val_score_dist.png")
 
-if __name__ == "main":
+if __name__ == "__main__":
   iterations = [0]
-  # create slrp plots for given iterations. each plot shows train and val slrp curves
-  # create_plots(iterations)
+  # # create slrp plots for given iterations for given experiment. each plot shows train and val slrp curves
+  # create_plots(iterations, "mask_rcnn_lvis_results")
 
   # obtain average s* statistics of given iterations
-  # count_settings = (True,True), (True,False), (False,True), (False,False)
-  # obtain_slrp_stats(iterations, count_settings)
+  # count_settings = [(True,True), (True,False), (False,True), (False,False)]
+  # obtain_slrp_stats(iterations, "mask_rcnn_lvis_results", count_settings)
 
-  # obtain_slrp_scatter_plots(iterations)
+  # obtain_slrp_scatter_plots(iterations, "mask_rcnn_lvis_results")
 
-  data_keys = ["dt_scores", "tps", "fps"]
-  dataset_types = ["val"]
-  obtain_score_distribution([0,1], data_keys, dataset_types)
+  # data_keys = ["dt_scores", "tps", "fps"]
+  # dataset_types = ["val"]
+  # obtain_score_distribution([0,1], data_keys, dataset_types)
